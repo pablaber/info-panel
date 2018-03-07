74ae16f23588e3c34cbab71e33af3196a84ba681
Patrick Bacon-Blaber <pablaber225@gmail.com>
2018-03-07 13:10:05 -0500

    allow CORS request to front end

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ var app = express();
 // Modules
 var nyw = require('./services/nyw-bus')
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function (req, res) {
   res.send('hello world');
 });
@@ -14,6 +20,6 @@ app.get('/nyw-bus-times', function (req, res) {
   });
 });
 
-app.listen(3000, function() {
-  console.log("Listening on port 3000...");
+app.listen(8080, function() {
+  console.log("Listening on port 8080...");
 });
