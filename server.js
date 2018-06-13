1e1de91b3b3eb8267c03e2e1f3a7e7a49ba2e5f9
Patrick Bacon-Blaber <pablaber225@gmail.com>
2018-06-13 18:10:23 -0400

    allow origin env

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var logger = require('./services/logger');
 const DEBUG = true;
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", process.env.ALLOW_ORIGIN);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
